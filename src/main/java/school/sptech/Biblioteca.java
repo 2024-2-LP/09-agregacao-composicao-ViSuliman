6595108fe350de6356f23e5a9cc8e314065d8635
ViSuliman <vitoria.suliman@sptech.school>
2024-10-24 19:51:45 -0300

    Exercicio completo

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,63 @@
 package school.sptech;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Biblioteca {
+    private String nome;
+    private List<Livro> livros;
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+        this.livros = new ArrayList<>();
+    }
+
+    public Biblioteca(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarLivro(Livro livro){
+        if(livro != null && livro.getTitulo() != null && !livro.getTitulo().isBlank() && livro.getAutor() != null && !livro.getAutor().isBlank() && livro.getDataPublicacao() != null){
+            livros.add(livro);
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo){
+
+        for (int i = 0; i < livros.size(); i++) {
+            if(livros.get(i).getTitulo().equalsIgnoreCase(titulo)){
+                livros.remove(i);
+            }
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo){
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getTitulo().equalsIgnoreCase(titulo)) {
+                return livros.get(i);
+            }
+        }
+        return null;
+    }
+
+    public Integer contarLivros(){
+       return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano){
+        List<Livro> livrosData = new ArrayList<>();
+
+        for (int i = 0; i < livros.size(); i++) {
+            if(ano >= livros.get(i).getDataPublicacao().getYear()){
+                livrosData.add(livros.get(i));
+            }
+        }
+        return livrosData;
+    }
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,84 @@
 package school.sptech;
 
-public class Livro {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Livro {
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes = new ArrayList<>();
+
+    public Livro(String titulo, String autor, LocalDate dataPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        this.avaliacoes = new ArrayList<>();
+    }
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas) {
+        if (descricao != null && !descricao.isBlank() && qtdEstrelas != null && qtdEstrelas >= 0 && qtdEstrelas <= 5) {
+            Avaliacao avaliacaoAtual = new Avaliacao(descricao, qtdEstrelas);
+            avaliacoes.add(avaliacaoAtual);
+        }
+    }
+
+    public Double calcularMediaAvaliacoes() {
+
+        if (avaliacoes.isEmpty()) {
+            return 0.0;
+        }
+            Double soma = 0.0;
+            for (int i = 0; i < avaliacoes.size(); i++) {
+                Avaliacao avaliacaoAtual = avaliacoes.get(i);
+                soma += avaliacaoAtual.getQtdEstrelas();
+            }
+            return soma / avaliacoes.size();
+        }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", dataPublicacao=" + dataPublicacao +
+                ", avaliacoes=" + avaliacoes +
+                '}';
+    }
 }
+
+
+
